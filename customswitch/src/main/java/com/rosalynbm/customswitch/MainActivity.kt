5d03860d88fd7a73a8a0e24a4c4a9797e56380f8
rosalynblanco <rblanco@adfone.com>
2022-10-17 17:37:18 -0400

    Custom switch
    - New 3 switches with custom drawables

--- customswitch/src/main/java/com/rosalynbm/customswitch/MainActivity.kt
+++ customswitch/src/main/java/com/rosalynbm/customswitch/MainActivity.kt
@@ -2,10 +2,33 @@ package com.rosalynbm.customswitch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.rosalynbm.customswitch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.customSwitch1.setOnCheckedChangeListener { _, isChecked ->
+            Log.d("Switch 1", "isChecked = $isChecked")
+        }
+
+        binding.customSwitch2.setOnCheckedChangeListener { _, isChecked ->
+            Log.d("Switch 2", "isChecked =  $isChecked")
+        }
+
+        binding.customSwitch3.setOnCheckedChangeListener { _, isChecked ->
+            Log.d("Switch 3", "isChecked =  $isChecked")
+        }
+
+        binding.customSwitch4.setOnCheckedChangeListener { _, isChecked ->
+            Log.d("Switch 4", "isChecked =  $isChecked")
+        }
+
     }
 }
